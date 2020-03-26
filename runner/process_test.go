f60da1460ff4dd6c56f2679109b4c4498811a648
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-03-26 13:39:37 -0400

    move copy test data into common testig tools

--- runner/process_test.go
+++ runner/process_test.go
@@ -2,37 +2,11 @@ package runner
 
 import (
 	"bytes"
-	"io/ioutil"
 	"os"
-	"path/filepath"
 	"testing"
 
-	"github.com/pkg/errors"
 	"github.com/pokt-network/pocket-runner/config"
 )
-
-// copyTestData will make a tempdir and then
-// "cp -r" a subdirectory under testdata there
-// returns the directory (which can now be used as config.Config.Home) and modified safely
-func copyTestData(subdir string) (string, error) {
-	tmpdir, err := ioutil.TempDir("", "pocket-runner-test")
-	if err != nil {
-		return "", errors.Wrap(err, "create temp dir")
-	}
-
-	src := filepath.Join("testdata", subdir)
-
-	options := Options{
-		Recursive: true,
-		Atomic:    true,
-	}
-	err = Copy(src, tmpdir, options)
-	if err != nil {
-		os.RemoveAll(tmpdir)
-		return "", errors.Wrap(err, "copying files")
-	}
-	return tmpdir, nil
-}
 
 func TestLaunchProcess(t *testing.T) {
 	home, err := copyTestData("validate")
