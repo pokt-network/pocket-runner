b44a3ed7d197018a1b512ef1664a2791fcae3758
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-04-03 19:15:44 -0400

    receive any type of input, output, errput for processess

--- main.go
+++ main.go
@@ -19,11 +19,12 @@ func main() {
 }
 
 func Run(args []string) error {
-	cfg, err := types.GetConfigFromEnv()
-	if err != nil {
-		return err
-	}
-	_, err = runner.LaunchProcess(cfg, args)
-	time.Sleep(time.Minute * 1) // wait for passphrase
+	// cfg, err := types.GetConfigFromEnv()
+	cfg := &types.Config{Home: "/Users/edjroz/Repos/pocket-runner/x/runner/testdata/validate", Name: "custom-core"}
+	// if err != nil {
+	// 	return err
+	// }
+	_, err := runner.LaunchProcess(cfg, args, os.Stdout, os.Stderr, os.Stdin)
+	time.Sleep(time.Second * 10)
 	return err
 }
