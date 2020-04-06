9025dec2108405e7f2eeb387d5d7fbd581bbe600
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-04-05 22:00:38 -0400

    register upgrade from tx event format

--- internal/types/upgrade_info.go
+++ internal/types/upgrade_info.go
@@ -1,8 +1,27 @@
 package types
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
 // UpgradeInfo is the details from the regexp
 type UpgradeInfo struct {
 	Name    string
-	Height  int
+	Height  int64
 	Version string
+}
+
+func (ui *UpgradeInfo) SetUpgrade(s string) error {
+	tx := strings.Split(s, " ")
+	height, err := strconv.Atoi(strings.TrimSuffix(tx[5], "]"))
+	if err != nil {
+		errors.Wrapf(err, "could not convert string: %s to integer", tx[5])
+	}
+	ui.Name = tx[2]
+	ui.Height = int64(height)
+	ui.Version = tx[2]
+	return nil
 }
