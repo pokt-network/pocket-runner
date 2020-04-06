5b22b5139acb8dfb2c1ab4d90ed0c687920c6e5c
Eduardo Diaz <eduardo.j.diaz.rodriguez@gmail.com>
2020-04-05 22:06:08 -0400

    refactor run to subscribe onto tx & block headers

--- main.go
+++ main.go
@@ -3,27 +3,90 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/pokt-network/pocket-runner/internal/types"
 	"github.com/pokt-network/pocket-runner/x/runner"
+	tmTypes "github.com/tendermint/tendermint/types"
 )
 
 func main() {
 	args := os.Args[1:]
-	err := Run(args)
+	Run(args)
+}
+
+func Run(args []string) {
+	cfg, err := types.GetConfigFromEnv()
 	if err != nil {
 		fmt.Printf("%+v\n", err)
 		os.Exit(1)
 	}
-}
-
-func Run(args []string) error {
-	cfg, err := types.GetConfigFromEnv()
-	if err != nil {
-		return err
-	}
-	_, err = runner.LaunchProcess(cfg, args, os.Stdout, os.Stderr, os.Stdin)
+	// Initial launcher, separated from loop due to passphrase
+	cmd, err := runner.LaunchProcess(cfg, args, os.Stdout, os.Stderr, os.Stdin)
 	time.Sleep(time.Second * 10)
-	return err
+
+	var eventListener *runner.EventListener
+	waitForUpgrade := func(errs chan error, upgrades chan *types.UpgradeInfo) {
+		for {
+			var upgrade *types.UpgradeInfo
+			select {
+			case rawTxEvt := <-eventListener.TxChan:
+				if len(rawTxEvt.Events["upgrade.action"]) == 1 {
+					if err := upgrade.SetUpgrade(strings.Join(rawTxEvt.Events["upgrade.action"], "")); err != nil {
+						errs <- err
+					}
+
+					if err := types.CheckBinary(cfg.UpgradeBin(upgrade.Name)); err != nil {
+						if cfg.AllowDownload {
+							// download
+						} else {
+							errs <- err
+						}
+					}
+					upgrades <- upgrade
+				}
+			}
+		}
+	}
+	waitForBlockHeight := func(errs chan error, upgrades chan *types.UpgradeInfo) {
+		for {
+			select {
+			case upgrade := <-upgrades:
+				for {
+					select {
+					case rawHeaderEvt := <-eventListener.HeaderChan:
+						headerEvt := rawHeaderEvt.Data.(tmTypes.EventDataNewBlockHeader)
+						if upgrade.Height != headerEvt.Header.Height {
+							continue
+						}
+						if err := cmd.Process.Kill(); err != nil {
+							errs <- err
+						}
+						if err := runner.Upgrade(cfg, upgrade); err != nil {
+							errs <- err
+						}
+						cmd, err = runner.LaunchProcess(cfg, args, os.Stdout, os.Stderr, os.Stdin)
+						if err != nil {
+							errs <- err
+						}
+					}
+				}
+			}
+		}
+	}
+
+	var errs chan error
+	var upgrades chan *types.UpgradeInfo
+
+	go waitForUpgrade(errs, upgrades)
+	go waitForBlockHeight(errs, upgrades)
+
+	for {
+		select {
+		case err := <-errs:
+			fmt.Printf("%+v\n", err)
+			os.Exit(1)
+		}
+	}
 }
